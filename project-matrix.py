c6878b66c150a34a8efc77fb971d11ea447e65dc
Arsya Aulia Amira <aauliaamira@student.ciputra.ac.id>
2024-12-19 07:06:50 +0800

    Modified project-matrix.py (Input baris kolom)

---
+++ project-matrix.py
@@ -0,0 +1,13 @@
+### INPUT BARIS KOLOM ###
+print ("Selamat Datang di Project Matrix")
+print ("Tentukan Baris dan Kolom")
+Baris = input (int("Baris = "))
+Input = input (int("Kolom = "))
+
+for range(len) in Baris:
+    input(int(f"Masukkan "))
+
+### PILIH MENU ####
+print ("Daftar Menu")
+print ("1. Penjumlahan Matrix")
+print ("2. Pengurangan Matrix")
